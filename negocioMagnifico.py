1fb4acde9059cb971faae049c1851fa53cca8441
pedro <pedrostahelazv2007@gmail.com>
2026-09-06 21:54:55 -0300

    Mandando tudo certinho

---
+++ negocioMagnifico.py
@@ -0,0 +1,33 @@
+import time
+
+mensagem1 = "Izabelly Mendonça Bello, eu preparei uma surpresa pra você. Não é das melhores, mas é para que a mulher da minha vida se sinta especial, assim como ela merece.\n"
+for letras in mensagem1:
+    print( letras, end = '', flush = True )
+    time.sleep(0.1)
+
+mensagem2 = "Izabelly Mendonça Bello, essa é a surpresa que eu tava falando que tava fazendo kkkkkkkkkk. Eu tô fazendo pra mostrar minhas habilidades computacionais muito legais e maneiras. Eu te amo muito e quero que você se sinta a pessoa mais especial do planeta todos os dias. Eu estou longe, mas quero me sentir próximo mesmo assim, com nossas ligações, mensagens e reels enviados(Inclusive, para de me humilhar em todos os Abril x Dezembro). Eu queria que você estivesse do meu lado agora e a que a gente ficasse deitado abraçadinho dando muitos beijos bons da porra(Delícia demais, daí agora é o momento que você pensa ""Tarado demaisss""... Sim, eu sou). Já falei aqui que te amo? Você é a MINHA cabrita e sempre será. Você é a minha inspiração pra ser alguém melhor todos os dias e a minha motivação pra sempre continuar melhorando, para que nós possamos ser muito ricos e eu o office do seu home lá na Itália ou Suíça. Então, encerro essa carta de maneira bobinha mandando um coração bem grande pra você, porque eu te amo demais(Não lembro se já disse que te amo, mas te amo). Beijos\n"
+time.sleep(5)
+print("\n")
+print("\n")
+
+for letras in mensagem2:
+    print( letras, end = '', flush = True )
+    time.sleep(0.1)
+
+time.sleep(2)
+print("\n")
+coracao = """
+        **       **
+      ******   ******
+    ********* *********
+   *********************
+   *********************
+    *******************
+      ***************
+        ***********
+          *******
+            ***
+             *
+"""
+
+print(coracao)
